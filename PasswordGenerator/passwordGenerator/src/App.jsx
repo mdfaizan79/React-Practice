b424ece2d30b6792a040932a08c8a031e793f75d
mdfaizan79 <[mdfaizan7707@gmail.com]>
2024-10-16 00:17:56 +0530

    Initial commit

--- PasswordGenerator/passwordGenerator/src/App.jsx
+++ PasswordGenerator/passwordGenerator/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
 
   return (
     <div className="h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white text-center p-8 rounded-lg shadow-lg">
+      <div className="bg-white text-center p-8 rounded-lg shadow-lg gap-4 w-90">
         <h1 className="text-4xl font-bold mb-4">Password Generator</h1>
         <input 
           type="text" 
@@ -43,28 +43,40 @@ function App() {
         >
           Copy
         </button>
-        <input 
-          type="range" 
-          min="4" 
-          max="20" 
-          value={length} 
-          onChange={(e) => setLength(e.target.value)} 
-          className="mt-4 w-full"
-        />
-        <input 
-          type="checkbox" 
-          checked={numAllow} 
-          onChange={(e) => setNumAllow(e.target.checked)} 
-        />
-        <label htmlFor="Number">Number</label>
-        <input 
-          type="checkbox" 
-          checked={charAllow} 
-          onChange={(e) => setCharAllow(e.target.checked)} 
-        />
-        <label htmlFor="Char">Character</label>
+        <div className="mt-8 w-full">
+          <label htmlFor="length" className="block text-left mb-2">Length: {length}</label>
+          <input 
+            type="range" 
+            id="length"
+            min="4" 
+            max="20" 
+            value={length} 
+            onChange={(e) => setLength(e.target.value)} 
+            className="w-full"
+          />
+        </div>
+        <div className="flex justify-between mt-4">
+          <label className="flex items-center">
+            <input 
+              type="checkbox" 
+              checked={numAllow} 
+              onChange={(e) => setNumAllow(e.target.checked)} 
+              className="mr-2"
+            />
+            Number
+          </label>
+          <label className="flex items-center">
+            <input 
+              type="checkbox" 
+              checked={charAllow} 
+              onChange={(e) => setCharAllow(e.target.checked)} 
+              className="mr-2"
+            />
+            Character
+          </label>
+        </div>
         <button 
-          className="bg-green-500 text-white px-4 py-2 rounded-lg mt-4"
+          className="bg-green-500 text-white px-4 py-2 rounded-lg mt-4 hover:bg-green-600 w-full"
           onClick={passwordGenerator}
         >
           Generate Password
